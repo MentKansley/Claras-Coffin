c4fc5bd2ba6ce1645a7688f82a6ec98f18793bae
Steven Emmanuel <ksteven925@gmail.com>
2018-01-16 22:42:20 +0530

    Basic Platforming
    Added a Controller2D, Player script.
    works great for flat & sloped surfaces.
    doesn't work for moving platforms.

---
+++ Clara's Coffin/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour {
+
+	public float jumpHeight = 4;
+	public float timeToJumpApex = 0.4f;
+	float acclerationTimeAirborne = 0.2f;
+	float acclerationTimeGrounded = 0.1f;
+	float moveSpeed = 6;
+
+	float gravity;
+	float jumpVelocity;
+	Vector3 velocity;
+	float velocityXSmooting;
+
+	Controller2D controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+
+		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+		print ("Gravity: " + gravity + " jump Velocity: " + jumpVelocity);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+
+		if(Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
+			velocity.y = jumpVelocity;
+		}
+
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmooting, (controller.collisions.below) ? acclerationTimeGrounded : acclerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+	}
+}
